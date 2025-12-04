cf74b37427f33eec314d93e90a7e3956225da1a3
yfZh <yfZhao495@outlook.com>
2025-12-04 11:33:01 +0900

    update

--- assets/export_paper.js
+++ assets/export_paper.js
@@ -1,12 +1,26 @@
-import fs from 'fs';
+// import fs from 'fs';
+// If running in a browser or server environment without direct file system access,
+// you should fetch the files via HTTP requests or use appropriate APIs.
 import path from 'path';
 import yaml from 'js-yaml';
 
-const authors_yml = fs.readFileSync(path.join(__dirname, '../_data/authors.yml'), 'utf8');
-const venues_yml = fs.readFileSync(path.join(__dirname, '../_data/venues.yml'), 'utf8');
-const international_confs_papers_yml = fs.readFileSync(path.join(__dirname, '../_data/international_c_papers.yml'), 'utf8');
-const domestic_confs_papers_yml = fs.readFileSync(path.join(__dirname, '../_data/domestic_c_papers.yml'), 'utf8');
-const preprints_yml = fs.readFileSync(path.join(__dirname, '../_data/preprints.yml'), 'utf8');
+import fileURLToPath from 'url';
+import dirname from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const authors_yml = await fetch(path.join(__dirname, '../_data/authors.yml')).then(res => res.text());
+const venues_yml = await fetch(path.join(__dirname, '../_data/venues.yml')).then(res => res.text());
+const international_confs_papers_yml = await fetch(path.join(__dirname, '../_data/international_c_papers.yml')).then(res => res.text());
+const domestic_confs_papers_yml = await fetch(path.join(__dirname, '../_data/domestic_c_papers.yml')).then(res => res.text());
+const preprints_yml = await fetch(path.join(__dirname, '../_data/preprints.yml')).then(res => res.text());
+
+// const authors_yml = fs.readFileSync(path.join(__dirname, '../_data/authors.yml'), 'utf8');
+// const venues_yml = fs.readFileSync(path.join(__dirname, '../_data/venues.yml'), 'utf8');
+// const international_confs_papers_yml = fs.readFileSync(path.join(__dirname, '../_data/international_c_papers.yml'), 'utf8');
+// const domestic_confs_papers_yml = fs.readFileSync(path.join(__dirname, '../_data/domestic_c_papers.yml'), 'utf8');
+// const preprints_yml = fs.readFileSync(path.join(__dirname, '../_data/preprints.yml'), 'utf8');
 
 const authors = yaml.load(authors_yml);
 const venues = yaml.load(venues_yml);
@@ -54,11 +68,5 @@ function generateExportPaper() {
     });
     return string;
 }
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // fs.writeFileSync(path.join(__dirname, 'exported_papers.txt'), generateExportPaper());
