096ead1fc601e3d9cd2ce19f279ec028734092d9
yfZh <yfZhao495@outlook.com>
2025-12-04 12:00:15 +0900

    update

--- assets/export_paper.js
+++ assets/export_paper.js
@@ -3,14 +3,14 @@
 // you should fetch the files via HTTP requests or use appropriate APIs.
 import yaml from "https://unpkg.com/js-yaml@4/dist/js-yaml.mjs";
 
-const venues_yml = await fetch('/data/venues.yml').then(res => res.text());
 const authors_yml = await fetch('/data/authors.yml').then(res => res.text());
+const venues_yml = await fetch('/data/venues.yml').then(res => res.text());
 const international_confs_papers_yml = await fetch('/data/international_c_papers.yml').then(res => res.text());
 const domestic_confs_papers_yml = await fetch('/data/domestic_c_papers.yml').then(res => res.text());
 const preprints_yml = await fetch('/data/preprints.yml').then(res => res.text());
 
-const authors = yaml.load(authors_yml);
 const venues = yaml.load(venues_yml);
+const authors = yaml.load(authors_yml);
 const international_confs_papers = yaml.load(international_confs_papers_yml);
 const domestic_confs_papers = yaml.load(domestic_confs_papers_yml);
 const preprints = yaml.load(preprints_yml);
